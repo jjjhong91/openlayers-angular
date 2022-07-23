0760870118741e29a78e8a23364516ee45585680
Joe Jhong <thank111222333@gmail.com>
2022-07-23 18:29:39 +0800

    建立基本圖資平台

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { ScalelineComponent } from './scaleline/scaleline.component';
+import { MousePositionComponent } from './mouse-position/mouse-position.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    ScalelineComponent,
+    MousePositionComponent
   ],
   imports: [
     BrowserModule
